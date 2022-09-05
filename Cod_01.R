2b51fde7261329a7214577e1fa75ad14125d82b4
jatrujillom <67253476+jatrujillom@users.noreply.github.com>
2022-09-05 17:02:43 -0500

    Update Cod_01.R

--- Cod_01.R
+++ Cod_01.R
@@ -126,3 +126,54 @@ df2 <- mutate(df, age2=age*age)
 regresion <- lm(y_total_m ~ age + age2 , data = df2)
 salida <- summary(regresion)
 tab_model(regresion)
+
+# Punto 2 - D  
+################################################################
+
+#CI por bootstrap
+require("tidyverse")
+set.seed(112)
+n<-length(df2$y_total_m)
+R<-1000 # Number of Repetions
+beta_regresion<-matrix(0,R,2)
+peak_age<-rep(0, R)
+dimnames(beta_regresion)<-list(c(1:R), c("age", "age2"))
+for (i in 1:R){
+  db_sample<- sample_frac(df2,size=1,replace=TRUE)
+  f<- lm(y_total_m ~ age + age2 , data = db_sample)
+  coefs<-f$coefficients
+  beta_regresion[i,]<-coefs[2:3]
+  peak_age[i]<-(-beta_regresion[i, 1])/(2*beta_regresion[i, 2])
+}
+varianza<-var(peak_age)
+
+#Predicci?n Age-Earnings
+age <- seq(18, 90, 0.1)
+pred <- predict(regresion, list(age=age, age2=age^2))
+
+#Grafica sin CI
+plot(age, pred, type = "l", xlab = "Edad", ylab = "Ingreso", lwd=2)
+mod<-lm(y_total_m ~ age + age2 , data = df2)
+beta<-mod$coefficients
+p <- c(-beta[2]/(2*beta[3]), max(pred))
+points(t(p), pch=16)
+
+#Predicci?n Age-Earnings CI
+age<-seq(30, 50, 0.1)
+pred<-predict(regresion, list(age=age, age2=age^2))
+
+#Grafica CI
+dev.new(width=100, height=max(pred)+200000)
+plot(age, pred, type = "l", xlab = "Edad", ylab = "Ingreso", lwd=2)
+mod<-lm(y_total_m ~ age + age2 , data = df2)
+beta<-mod$coefficients
+p <- c(-beta[2]/(2*beta[3]), max(pred))
+points(t(p), pch=16)
+ps <- matrix(c(1, 15/p[2], p[2]/p[1], 1), nrow=2, ncol=2)*p 
+points(rbind(c(round(-beta[2]/(2*beta[3])+(1.96*sqrt(varianza)), digits = 2), min(pred)-3000), c(round(-beta[2]/(2*beta[3])-(1.96*sqrt(varianza)), digits = 2), min(pred)-3000), ps[1,]), pch=c("|", "|", "|"), cex=c(1,1,1))
+text(rbind(c(round(-beta[2]/(2*beta[3])+(1.96*sqrt(varianza)), digits = 2), min(pred)-3000), c(round(-beta[2]/(2*beta[3])-(1.96*sqrt(varianza)), digits = 2), min(pred)-3000), ps[1,]), col="black", labels=c("UB", "LB", round(ps[1], digits = 2)), pos=c(3, 3, 1))
+
+#Intervalo de Confianza
+CI<-matrix(c(-beta[2]/(2*beta[3])-(1.96*sqrt(varianza)), -beta[2]/(2*beta[3])+(1.96*sqrt(varianza))), nrow = 1, ncol = 2)
+colnames(CI)<-c("Lower Bound", "Upper_Bound")
+CI
